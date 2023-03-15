ba728a4eb1242c10166212f0ef766d6f197f163a
Welersonfrr <contato.welerson@gmail.com>
2023-03-15 11:00:29 -0300

    Header component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <Router>
+//     <StateProvider initialState={initialState} reducer={reducer}>
+//       <App />
+//     </StateProvider>
+//   </Router>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <React.StrictMode>
   <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </Router>
+  // </React.StrictMode>
 );
